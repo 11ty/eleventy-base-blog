edf16ab7e480eec0237660f75ef01a0ed4f64608
Malte Ubl <malte.ubl@gmail.com>
2020-07-07 19:27:56 -0700

    fns

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
+function prefetch(e) {
+  if (e.target.tagName != "A") {
+    return;
+  }
+  var l = document.createElement("link");
+  l.rel = "prefetch";
+  l.href = e.target.href;
+  document.head.appendChild(l);
 }
-gtag("js", new Date());
-gtag("config", "UA-141920860-1");
+document.documentElement.addEventListener("mouseover", prefetch, {
+  capture: true,
+  passive: true,
+});
+document.documentElement.addEventListener("touchstart", prefetch, {
+  capture: true,
+  passive: true,
+});
+
+window.ga =
+  window.ga ||
+  function () {
+    (ga.q = ga.q || []).push(arguments);
+  };
+ga.l = +new Date();
+ga("create", "UA-141920860-1", "auto");
+ga("send", "pageview");
+
 setInterval(time, 60 * 1000);
 function time() {
   if (document.hidden) return;
-  gtag("event", "time", {
-    event_category: "time",
-    event_label: "seconds",
-    value: 60,
+  ga("send", {
+    hitType: "event",
+    eventCategory: "time",
+    eventAction: "seconds",
+    eventLabel: 60,
   });
 }
