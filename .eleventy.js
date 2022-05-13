8771842fed6314500fd9aea7a0af87c82399dd28
Zach Leatherman <zachleatherman@gmail.com>
2022-05-13 16:10:00 -0500

    Update deps

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
-const { DateTime } = require("luxon");
 const fs = require("fs");
+
+const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
 
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
