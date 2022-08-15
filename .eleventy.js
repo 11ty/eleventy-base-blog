df6a44bf805daa58eb4d0333706befee279ed000
Zach Leatherman <zachleatherman@gmail.com>
2022-08-15 14:53:45 -0500

    Small tweaks to the styling. Uses a global max width on the theme

--- .eleventy.js
+++ .eleventy.js
@@ -28,7 +28,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL yyyy");
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
--- en/blog/secondpost.md
+++ en/blog/secondpost.md
@@ -1,5 +1,5 @@
 ---
-title: This is my second post.
+title: This is my second post with a much longer title.
 description: This is a post on My Blog about leveraging agile frameworks.
 date: 2018-07-04
 tags:
