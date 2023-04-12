7082ddbdca3139e06f12be3562348ab993b7929c
Zach Leatherman <zachleatherman@gmail.com>
2023-04-12 16:40:18 -0500

    Remove unnecessary quotes

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
-	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
-	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+	title: "Eleventy Base Blog v8",
+	url: "https://example.com/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Your Name Here",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
 	}
 }
