f30c79c0d9998355bfdb3e4f4988d4e986149771
Zearin <Zearin@users.noreply.github.com>
2023-05-04 16:36:08 -0400

    Add JSDoc (for completion) eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,6 +10,7 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const pluginDrafts = require("./eleventy.config.drafts.js");
 const pluginImages = require("./eleventy.config.images.js");
 
+/** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
