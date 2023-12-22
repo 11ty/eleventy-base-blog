0676bc6d4af2a19df0922236f2dc49cbb5a247e4
Yahya J. Aifit <yahya.junianto@bukalapak.com>
2023-12-22 22:39:32 +0700

    checkpoint

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,7 +14,8 @@ module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
-		"./public/": "/",
+		"./public/css/*.css": "/css/",
+		"./public/img": "/",
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
 	});
 
