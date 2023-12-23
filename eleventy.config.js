393bc300a76d1b54ec0f6c96f592ee10574dd50e
Yahya J.Aifit <yahya@yja.me>
2023-12-24 00:57:07 +0700

    revert addPassthroughCopy

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,8 +14,7 @@ module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
-		"./public/css/*.css": "/css/",
-		"./public/img": "/",
+		"./public/": "/",
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
 	});
 
