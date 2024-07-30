2128a691d9c17269e597cff9dfb35daf677f77dd
Jackson Goode <jacksongoode@proton.me>
2024-07-30 15:43:03 -0700

    No need

--- eleventy.config.js
+++ eleventy.config.js
@@ -19,7 +19,6 @@ module.exports = function (eleventyConfig) {
 	});
 	eleventyConfig.addPassthroughCopy({ "css": "css" });
 	eleventyConfig.addPassthroughCopy({ "js": "js" });
-	eleventyConfig.addPassthroughCopy("_headers");
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
