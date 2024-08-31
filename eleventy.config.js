97c69f7a891890b4ab8ceb74fddb05814f29ba86
Jackson Goode <jacksongoode@proton.me>
2024-08-31 12:33:34 -0700

    Add reset

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,9 +6,15 @@ module.exports = async function (eleventyConfig) {
 	const { default: markdownItAnchor } = await import("markdown-it-anchor");
 
 	const { default: pluginRss } = await import("@11ty/eleventy-plugin-rss");
-	const { default: pluginSyntaxHighlight } = await import("@11ty/eleventy-plugin-syntaxhighlight");
-	const { default: pluginBundle } = await import("@11ty/eleventy-plugin-bundle");
-	const { default: pluginNavigation } = await import("@11ty/eleventy-navigation");
+	const { default: pluginSyntaxHighlight } = await import(
+		"@11ty/eleventy-plugin-syntaxhighlight"
+	);
+	const { default: pluginBundle } = await import(
+		"@11ty/eleventy-plugin-bundle"
+	);
+	const { default: pluginNavigation } = await import(
+		"@11ty/eleventy-navigation"
+	);
 	const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
 
 	const { default: pluginDrafts } = await import("./eleventy.config.drafts.js");
@@ -38,7 +44,7 @@ module.exports = async function (eleventyConfig) {
 	// Official plugins
 	eleventyConfig.addPlugin(pluginRss);
 	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-		preAttributes: { tabindex: 0 }
+		preAttributes: { tabindex: 0 },
 	});
 	eleventyConfig.addPlugin(pluginNavigation);
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
@@ -56,12 +62,14 @@ module.exports = async function (eleventyConfig) {
 	// Filters
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
+			format || "dd LLLL yyyy",
+		);
 	});
 
-	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
 	});
 
 	// Get the first `n` elements of a collection.
@@ -82,20 +90,22 @@ module.exports = async function (eleventyConfig) {
 	});
 
 	// Return all the tags used in a collection
-	eleventyConfig.addFilter("getAllTags", collection => {
+	eleventyConfig.addFilter("getAllTags", (collection) => {
 		let tagSet = new Set();
 		for (let item of collection) {
-			(item.data.tags || []).forEach(tag => tagSet.add(tag));
+			(item.data.tags || []).forEach((tag) => tagSet.add(tag));
 		}
 		return Array.from(tagSet);
 	});
 
 	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-		return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+		return (tags || []).filter(
+			(tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1,
+		);
 	});
 
 	// Customize Markdown library settings:
-	eleventyConfig.amendLibrary("md", mdLib => {
+	eleventyConfig.amendLibrary("md", (mdLib) => {
 		mdLib.use(markdownItAnchor, {
 			permalink: markdownItAnchor.permalink.ariaHidden({
 				placement: "after",
@@ -104,43 +114,46 @@ module.exports = async function (eleventyConfig) {
 				ariaHidden: false,
 			}),
 			level: [1, 2, 3, 4],
-			slugify: eleventyConfig.getFilter("slugify")
+			slugify: eleventyConfig.getFilter("slugify"),
 		});
 	});
 
 	// Shortcodes
 	eleventyConfig.addShortcode("currentBuildDate", () => {
-		return (new Date()).toISOString();
-	})
+		return new Date().toISOString();
+	});
 
-	eleventyConfig.addShortcode("figure", function (src, caption, width = '100%') {
-		const getMediaElement = () => {
-			if (src.includes('youtube.com') || src.includes('youtu.be')) {
-				const [baseUrl, params] = src.split('?');
-				const videoId = baseUrl.split('v=')[1] || baseUrl.split('/').pop();
-				const youtubeParams = params ? `params="${params}"` : '';
+	eleventyConfig.addShortcode(
+		"figure",
+		function (src, caption, width = "100%") {
+			const getMediaElement = () => {
+				if (src.includes("youtube.com") || src.includes("youtu.be")) {
+					const [baseUrl, params] = src.split("?");
+					const videoId = baseUrl.split("v=")[1] || baseUrl.split("/").pop();
+					const youtubeParams = params ? `params="${params}"` : "";
 
-				return `
+					return `
 					<lite-youtube videoid="${videoId}" ${youtubeParams} style="background-image: url('https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg');" title="${caption}">
 						<a href="${src}" class="lty-playbtn" title="${caption}">
 							<span class="lyt-visually-hidden">${caption}</span>
 						</a>
 					</lite-youtube>`;
-			}
+				}
 
-			const extension = src.split('.').pop().toLowerCase();
-			if (['mp4', 'webm', 'ogg'].includes(extension)) {
-				return `<video src="${src}" width="${width}" controls>Your browser does not support the video tag.</video>`;
-			}
+				const extension = src.split(".").pop().toLowerCase();
+				if (["mp4", "webm", "ogg"].includes(extension)) {
+					return `<video src="${src}" width="${width}" controls>Your browser does not support the video tag.</video>`;
+				}
 
-			return `<img src="${src}" width="${width}" alt="${caption}" />`;
-		};
+				return `<img src="${src}" width="${width}" alt="${caption}" />`;
+			};
 
-		return `<figure>
+			return `<figure>
 			${getMediaElement()}
 			<figcaption>${caption}</figcaption>
 		</figure>`;
-	});
+		},
+	);
 
 	// Features to make your build faster (when you need them)
 
@@ -153,12 +166,7 @@ module.exports = async function (eleventyConfig) {
 	return {
 		// Control which files Eleventy will process
 		// e.g.: *.md, *.njk, *.html, *.liquid
-		templateFormats: [
-			"md",
-			"njk",
-			"html",
-			"liquid",
-		],
+		templateFormats: ["md", "njk", "html", "liquid"],
 
 		// Pre-process *.md files with: (default: `liquid`)
 		markdownTemplateEngine: "njk",
@@ -168,10 +176,10 @@ module.exports = async function (eleventyConfig) {
 
 		// These are all optional:
 		dir: {
-			input: "content",          // default: "."
-			includes: "../_includes",  // default: "_includes"
-			data: "../_data",          // default: "_data"
-			output: "_site"
+			input: "content", // default: "."
+			includes: "../_includes", // default: "_includes"
+			data: "../_data", // default: "_data"
+			output: "_site",
 		},
 
 		// -----------------------------------------------------------------
