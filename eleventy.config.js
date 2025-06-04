a521dfadefc567c14960dc04a4b7dea8e0f86f3c
Zach Leatherman <zachleat@users.noreply.github.com>
2025-06-04 08:43:12 -0500

    Comment clarity

--- eleventy.config.js
+++ eleventy.config.js
@@ -35,16 +35,16 @@ export default async function(eleventyConfig) {
 	// Bundle <style> content and adds a {% css %} paired shortcode
 	eleventyConfig.addBundle("css", {
 		toFileDirectory: "dist",
-		// Add all <style> content to `css` bundle (use eleventy:ignore to opt-out)
-		// supported selectors: https://www.npmjs.com/package/posthtml-match-helper
+		// Add all <style> content to `css` bundle (use <style eleventy:ignore> to opt-out)
+		// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
 		bundleHtmlContentFromSelector: "style",
 	});
 
 	// Bundle <script> content and adds a {% js %} paired shortcode
 	eleventyConfig.addBundle("js", {
 		toFileDirectory: "dist",
-		// Add all <script> content to the `js` bundle (use eleventy:ignore to opt-out)
-		// supported selectors: https://www.npmjs.com/package/posthtml-match-helper
+		// Add all <script> content to the `js` bundle (use <script eleventy:ignore> to opt-out)
+		// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
 		bundleHtmlContentFromSelector: "script",
 	});
 
