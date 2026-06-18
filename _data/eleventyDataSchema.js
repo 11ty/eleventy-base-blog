e37a55c4c4705d7881929de5242ec50448b5eb0b
Zach Leatherman <zachleat@users.noreply.github.com>
2026-06-18 09:21:12 -0500

    Fixes #249

--- _data/eleventyDataSchema.js
+++ _data/eleventyDataSchema.js
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import { fromZodError } from 'zod-validation-error';
 
 // Draft content, validate `draft` front matter
 export default function() {
@@ -7,11 +6,11 @@ export default function() {
 		// Note that drafts may be skipped in a preprocessor (see eleventy.config.js)
 		// when doing a standard build (not --serve or --watch)
 		let result = z.object({
-			draft: z.boolean().or(z.undefined()),
+			draft: z.coerce.boolean().default(false)
 		}).safeParse(data);
 
 		if(result.error) {
-			throw fromZodError(result.error);
+			throw result.error;
 		}
 	}
 }
